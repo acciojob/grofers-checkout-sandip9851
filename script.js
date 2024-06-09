699ffbc191a6b145f2efa6a1cd21cb945941244c
Sandip  Pradhan <158505928+sandip9851@users.noreply.github.com>
2024-06-09 18:14:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,21 +4,17 @@ document.body.appendChild(getSumBtn);
 const prices = document.querySelectorAll(".price")
 const table = document.querySelector("table")
 
-const getSum = () => 
-	let sum = 0;
-//Add your code here
+const getSum = () => {
+	let sum = 0; // reset sum each time the button is clicked
 	for (let val of prices) {
 		let value = Number(val.innerText) ;
 		sum += value;
 	}
-const row = document.createElement("tr");
+	const row = document.createElement("tr");
 	row.innerHTML = `
 	<td>Total Price</td>
       <td>${sum}</td>`
-	
-  table.append(row)
-sum = 0;
+	table.append(row)
 };
 
 getSumBtn.addEventListener("click", getSum);
-
